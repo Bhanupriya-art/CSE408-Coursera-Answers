52095a21c74d05be9963f232c02fb5102e2dae49
Bhanu Priya <120407422+Bhanupriya-art@users.noreply.github.com>
2024-03-14 01:24:53 +0530

    Commit

--- Dynamic Programming, Greedy Algorithms/Week 3/Problem Set 3.py
+++ Dynamic Programming, Greedy Algorithms/Week 3/Problem Set 3.py
@@ -258,36 +258,6 @@ print('All tests passed: 15 points!')
 
 # YOUR ANSWER HERE
 
-# In[6]:
-
-
-def do_test(times, m, expected):
-    (a, makespan) = greedy_makespan_min(times,m )
-    print('\t Assignment returned: ', a)
-    print('\t Claimed makespan: ', makespan)
-    assert compute_makespan(times, m, a) == makespan, 'Assignment returned is not consistent with the reported makespan'
-    assert makespan == expected, f'Expected makespan should be {expected}, your core returned {makespan}'
-    print('Passed')
-print('Test 1:')
-times = [2, 2, 2, 2, 2, 2, 2, 2, 3] 
-m = 3
-expected = 7
-do_test(times, m, expected)
-
-print('Test 2:')
-times = [1]*20 + [5]
-m = 5
-expected =9
-do_test(times, m, expected)
-
-print('Test 3:')
-times = [1]*40 + [2]
-m = 20
-expected = 4
-do_test(times, m, expected)
-print('All tests passed: 15 points!')
-
-
 # ## Analysis of Greedy Makespan Minimization 
 
 # We will now analyze the greedy algorithm and show the following result.
